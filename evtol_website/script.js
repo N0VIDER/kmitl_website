3fe1d3b7deb61b13dc03286338d3d8fd2096ebd5
N0VIDER <70048671+N0VIDER@users.noreply.github.com>
2023-11-20 04:55:34 +0700

    Add files via upload

---
+++ evtol_website/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var backgroundVideo = document.getElementById('backgroundVideo');
+    backgroundVideo.playbackRate = 0.8; // Slow down the video to half its original speed
+});
