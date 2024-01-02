f782a40f552c279247240889064463310a756bee
N0VIDER <70048671+N0VIDER@users.noreply.github.com>
2024-01-02 21:46:20 +0700

    Add files via upload

--- evtol_website/script.js
+++ evtol_website/script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var backgroundVideo = document.getElementById('backgroundVideo');
     backgroundVideo.playbackRate = 0.8; // Slow down the video to half its original speed
+
+    document.getElementById('catalogueButton').addEventListener('click', function() {
+        console.log("Button clicked");
+        var img = document.createElement('img');
+        img.src = 'catalogue.png'; // Replace with your image path
+        document.body.appendChild(img);
+    });
 });
